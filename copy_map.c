b1f32cbfd64761ea6fc1bab5c3cf9f087bbe581d
rzafari42 <redwane.zafari@hotmail.com>
2020-04-27 20:33:24 +0200

    NORME

--- copy_map.c
+++ copy_map.c
@@ -6,64 +6,64 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 18:30:32 by marvin            #+#    #+#             */
-/*   Updated: 2020/04/27 18:30:32 by marvin           ###   ########.fr       */
+/*   Updated: 2020/04/27 20:25:59 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void    ft_copy_maptwo(t_deflibx *mlx, int fd, char *line)
+void	ft_copy_maptwo(t_deflibx *mlx, int fd, char *line)
 {
-    int i;
-    int ret;
-    char *temp;
+	int i;
+	int ret;
+	char *temp;
 
-    i = 0;
-    temp = NULL;
-    temp = mlx->parse.map[i];
-    mlx->parse.map[i] = ft_strdup_zero(line, mlx);
-    free(temp);
-    free(line);
-    i++;
-    while ((ret = get_next_line(fd, &line)) > -1)
-    {
-        if (line[0] != '\0')
-        {
-            temp = mlx->parse.map[i];
-            mlx->parse.map[i] = ft_strdup_zero(line, mlx);
-            free (temp);
-            i++;
-        }
-        free(line);
-        if (ret == 0)
-            break;
-    }
-    close(fd);
+	i = 0;
+	temp = NULL;
+	temp = mlx->parse.map[i];
+	mlx->parse.map[i] = ft_strdup_zero(line, mlx);
+	free(temp);
+	free(line);
+	i++;
+	while ((ret = get_next_line(fd, &line)) > -1)
+	{
+		if (line[0] != '\0')
+		{
+			temp = mlx->parse.map[i];
+			mlx->parse.map[i] = ft_strdup_zero(line, mlx);
+			free (temp);
+			i++;
+		}
+		free(line);
+		if (ret == 0)
+			break;
+	}
+	close(fd);
 }
 
-void    ft_copy_map(t_deflibx *mlx)
+void	ft_copy_map(t_deflibx *mlx)
 {
-    int ret;
-    int fd;
-    char *line;
+	int ret;
+	int fd;
+	char *line;
 
-    ret = 0;
-    if (!(line = (char*)malloc(sizeof(char))))
-    {
-        ft_free_map(mlx);
-        ft_return("MALLOC ERROR :(", mlx);
-    }
-    line[0] = 1;
-    if ((fd = open(mlx->parse.files, O_RDONLY)) < 0)
-    {
-        free(line);
-        ft_free_map(mlx);
-        ft_return("fd error in ft_copy_map", mlx);
-    }
-    while (!ft_check_wall(line))
-    {
-        free(line);
-        ret = get_next_line(fd, &line);
-    }
-    ft_copy_maptwo(mlx, fd, line);
+	ret = 0;
+	if (!(line = (char*)malloc(sizeof(char))))
+	{
+		ft_free_map(mlx);
+		ft_return("MALLOC ERROR :(", mlx);
+	}
+	line[0] = 1;
+	if ((fd = open(mlx->parse.files, O_RDONLY)) < 0)
+	{
+		free(line);
+		ft_free_map(mlx);
+		ft_return("fd error in ft_copy_map", mlx);
+	}
+	while (!ft_check_wall(line))
+	{
+		free(line);
+		ret = get_next_line(fd, &line);
+	}
+	ft_copy_maptwo(mlx, fd, line);
 }
--- game_modes.c
+++ game_modes.c
@@ -6,99 +6,95 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 11:47:42 by rzafari           #+#    #+#             */
-/*   Updated: 2020/04/15 01:02:48 by marvin           ###   ########.fr       */
+/*   Updated: 2020/04/27 20:33:04 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void ft_normal_mode(t_deflibx *mlx)
+void	ft_normal_mode(t_deflibx *mlx)
 {
- if(mlx->raycast.sideDistX < mlx->raycast.sideDistY)
-    {
-    mlx->raycast.sideDistX += mlx->raycast.deltaDistX;
-    mlx->raycast.mapX += mlx->raycast.stepX;
-    if (mlx->raycast.rayDirX < 0)
-        mlx->raycast.side = 0;
-    if (mlx->raycast.rayDirX > 0)
-        mlx->raycast.side = 1;
-    }
-    else
-    {
-    mlx->raycast.sideDistY += mlx->raycast.deltaDistY;
-    mlx->raycast.mapY += mlx->raycast.stepY;
-    if (mlx->raycast.rayDirY < 0)
-        mlx->raycast.side = 2;
-    if (mlx->raycast.rayDirY > 0)
-        mlx->raycast.side = 3;
-    }
-    //gives every side of a cube, different colors upon the ray directon
-    if(mlx->raycast.side == 0)
-    {
-    mlx->color.r = 255;
-    mlx->color.g = 0;
-    mlx->color.b = 0;
-    mlx->text.img_textdata_utils = mlx->text.img_textdata0;
-    }
-    if(mlx->raycast.side == 1) 
-    {
-    mlx->color.r = 208;
-    mlx->color.g = 58;
-    mlx->color.b = 8;
-    mlx->text.img_textdata_utils = mlx->text.img_textdata1;
-    }
-    if(mlx->raycast.side == 2) 
-    {
-    mlx->color.r = 123;
-    mlx->color.g = 11;
-    mlx->color.b = 8;
-    mlx->text.img_textdata_utils = mlx->text.img_textdata2;
-    }
-    if(mlx->raycast.side == 3)
-    {
-    mlx->color.r = 123;
-    mlx->color.g = 0;
-    mlx->color.b = 54;
-    mlx->text.img_textdata_utils = mlx->text.img_textdata3;
-    }
-    /*mlx->color.floorcolor = 4341824;
-    mlx->color.ceilcolor = 4341824;*/
-    mlx->color.color = convertrgbtoint(mlx->color.r, mlx->color.g, mlx->color.b);
+	if (mlx->raycast.sideDistX < mlx->raycast.sideDistY)
+	{
+		mlx->raycast.sideDistX += mlx->raycast.deltaDistX;
+		mlx->raycast.mapX += mlx->raycast.stepX;
+		if (mlx->raycast.rayDirX < 0)
+			mlx->raycast.side = 0;
+		if (mlx->raycast.rayDirX > 0)
+			mlx->raycast.side = 1;
+	}
+	else
+	{
+		mlx->raycast.sideDistY += mlx->raycast.deltaDistY;
+		mlx->raycast.mapY += mlx->raycast.stepY;
+		if (mlx->raycast.rayDirY < 0)
+			mlx->raycast.side = 2;
+		if (mlx->raycast.rayDirY > 0)
+			mlx->raycast.side = 3;
+	}
+	if (mlx->raycast.side == 0)
+	{
+		mlx->color.r = 255;
+		mlx->color.g = 0;
+		mlx->color.b = 0;
+		mlx->text.img_textdata_utils = mlx->text.img_textdata0;
+	}
+	if (mlx->raycast.side == 1)
+	{
+		mlx->color.r = 208;
+		mlx->color.g = 58;
+		mlx->color.b = 8;
+		mlx->text.img_textdata_utils = mlx->text.img_textdata1;
+	}
+	if (mlx->raycast.side == 2)
+	{
+		mlx->color.r = 123;
+		mlx->color.g = 11;
+		mlx->color.b = 8;
+		mlx->text.img_textdata_utils = mlx->text.img_textdata2;
+	}
+	if (mlx->raycast.side == 3)
+	{
+		mlx->color.r = 123;
+	       	mlx->color.g = 0;
+		mlx->color.b = 54;
+		mlx->text.img_textdata_utils = mlx->text.img_textdata3;
+	}
+	mlx->color.color = convertrgbtoint(mlx->color.r, mlx->color.g, mlx->color.b);
 }
 
-void ft_electro_mode(t_deflibx *mlx)
+void	ft_electro_mode(t_deflibx *mlx)
 {
-    //jump to next map square, OR in x-direction, OR in y-direction
-    if(mlx->raycast.sideDistX < mlx->raycast.sideDistY)
-    {
-    mlx->raycast.sideDistX += mlx->raycast.deltaDistX;
-    mlx->raycast.mapX += mlx->raycast.stepX;
-    if (mlx->raycast.rayDirX < 0)
-    {
-        mlx->raycast.side = 0;
-        mlx->color.color = mlx->color.color / 2;
-    }
-    if (mlx->raycast.rayDirX > 0)
-    {
-        mlx->raycast.side = 1;
-        mlx->color.color = mlx->color.color / 3;
-    }
-    }
-    else
-    {
-    mlx->raycast.sideDistY += mlx->raycast.deltaDistY;
-    mlx->raycast.mapY += mlx->raycast.stepY;
-    if (mlx->raycast.rayDirY < 0)
-    {
-        mlx->raycast.side = 2;
-        mlx->color.color = mlx->color.color / 4;
-    }
-    if (mlx->raycast.rayDirY > 0)
-    {
-        mlx->raycast.side = 3;
-        mlx->color.color = mlx->color.color / 5;
-    }
-    }
-    mlx->color.floorcolor = 0;
-    mlx->color.ceilcolor = 0;
+	if (mlx->raycast.sideDistX < mlx->raycast.sideDistY)
+	{
+		mlx->raycast.sideDistX += mlx->raycast.deltaDistX;
+		mlx->raycast.mapX += mlx->raycast.stepX;
+		if (mlx->raycast.rayDirX < 0)
+		{
+			mlx->raycast.side = 0;
+			mlx->color.color = mlx->color.color / 2;
+		}
+		if (mlx->raycast.rayDirX > 0)
+		{
+			mlx->raycast.side = 1;
+			mlx->color.color = mlx->color.color / 3;
+		}
+	}
+	else
+	{
+		mlx->raycast.sideDistY += mlx->raycast.deltaDistY;
+		mlx->raycast.mapY += mlx->raycast.stepY;
+		if (mlx->raycast.rayDirY < 0)
+		{
+			mlx->raycast.side = 2;
+			mlx->color.color = mlx->color.color / 4;
+		}
+		if (mlx->raycast.rayDirY > 0)
+		{
+			mlx->raycast.side = 3;
+			mlx->color.color = mlx->color.color / 5;
+		}
+	}
+	mlx->color.floorcolor = 0;
+	mlx->color.ceilcolor = 0;
 }

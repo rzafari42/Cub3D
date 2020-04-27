9904f097beb63580ec15ea154e713ba7adb46ff4
rzafari42 <redwane.zafari@hotmail.com>
2020-04-27 21:32:19 +0200

    gamemode

--- game_modes.c
+++ game_modes.c
@@ -6,11 +6,38 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 11:47:42 by rzafari           #+#    #+#             */
-/*   Updated: 2020/04/27 20:33:04 by rzafari          ###   ########.fr       */
+/*   Updated: 2020/04/27 21:31:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	ft_normal_modenext(t_deflibx *mlx)
+{
+	if (mlx->raycast.side == 1)
+	{
+		mlx->color.r = 208;
+		mlx->color.g = 58;
+		mlx->color.b = 8;
+		mlx->text.img_textdata_utils = mlx->text.img_textdata1;
+	}
+	if (mlx->raycast.side == 2)
+	{
+		mlx->color.r = 123;
+		mlx->color.g = 11;
+		mlx->color.b = 8;
+		mlx->text.img_textdata_utils = mlx->text.img_textdata2;
+	}
+	if (mlx->raycast.side == 3)
+	{
+		mlx->color.r = 123;
+	       	mlx->color.g = 0;
+		mlx->color.b = 54;
+		mlx->text.img_textdata_utils = mlx->text.img_textdata3;
+	}
+	mlx->color.color = convertrgbtoint(mlx->color.r,
+		mlx->color.g, mlx->color.b);
+}
 
 void	ft_normal_mode(t_deflibx *mlx)
 {
@@ -39,28 +66,23 @@ void	ft_normal_mode(t_deflibx *mlx)
 		mlx->color.b = 0;
 		mlx->text.img_textdata_utils = mlx->text.img_textdata0;
 	}
-	if (mlx->raycast.side == 1)
-	{
-		mlx->color.r = 208;
-		mlx->color.g = 58;
-		mlx->color.b = 8;
-		mlx->text.img_textdata_utils = mlx->text.img_textdata1;
-	}
-	if (mlx->raycast.side == 2)
-	{
-		mlx->color.r = 123;
-		mlx->color.g = 11;
-		mlx->color.b = 8;
-		mlx->text.img_textdata_utils = mlx->text.img_textdata2;
-	}
-	if (mlx->raycast.side == 3)
-	{
-		mlx->color.r = 123;
-	       	mlx->color.g = 0;
-		mlx->color.b = 54;
-		mlx->text.img_textdata_utils = mlx->text.img_textdata3;
-	}
-	mlx->color.color = convertrgbtoint(mlx->color.r, mlx->color.g, mlx->color.b);
+	ft_electro_modenext(mlx);
+}
+
+void	ft_electro_modenext(t_deflibx *mlx)
+{
+		mlx->raycast.sideDistY += mlx->raycast.deltaDistY;
+		mlx->raycast.mapY += mlx->raycast.stepY;
+		if (mlx->raycast.rayDirY < 0)
+		{
+			mlx->raycast.side = 2;
+			mlx->color.color = mlx->color.color / 4;
+		}
+		if (mlx->raycast.rayDirY > 0)
+		{
+			mlx->raycast.side = 3;
+			mlx->color.color = mlx->color.color / 5;
+		}
 }
 
 void	ft_electro_mode(t_deflibx *mlx)
@@ -81,20 +103,7 @@ void	ft_electro_mode(t_deflibx *mlx)
 		}
 	}
 	else
-	{
-		mlx->raycast.sideDistY += mlx->raycast.deltaDistY;
-		mlx->raycast.mapY += mlx->raycast.stepY;
-		if (mlx->raycast.rayDirY < 0)
-		{
-			mlx->raycast.side = 2;
-			mlx->color.color = mlx->color.color / 4;
-		}
-		if (mlx->raycast.rayDirY > 0)
-		{
-			mlx->raycast.side = 3;
-			mlx->color.color = mlx->color.color / 5;
-		}
-	}
+		ft_electro_modenext(mlx);
 	mlx->color.floorcolor = 0;
 	mlx->color.ceilcolor = 0;
 }

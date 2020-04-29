ad9fe26d56d7de5254bae0ad414a899e7bb8d867
rzafari42 <rzafari@student.42.fr>
2020-04-29 18:10:37 +0200

    norme

--- check_maptwo.c
+++ check_maptwo.c
@@ -1,19 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_maptwo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzafari42 <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/29 18:05:36 by rzafari42         #+#    #+#             */
+/*   Updated: 2020/04/29 18:10:21 by rzafari42        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "cub3d.h"
 
 void	ft_check_mapfournext(t_deflibx *mlx, int i, int j)
 {
-    while (mlx->parse.map[i][j] != '1')
-    {
-        if ((mlx->parse.map[i][j] != ' ') ||
-        (mlx->parse.map[i - 1][j] == '0' ||
-        mlx->parse.map[i + 1][j] == '0'))
-        {
-            ft_free_map(mlx);
-            ft_return("Map not closed", mlx);
-        }
-        j--;
-    }
+	while (mlx->parse.map[i][j] != '1')
+	{
+		if ((mlx->parse.map[i][j] != ' ') ||
+		(mlx->parse.map[i - 1][j] == '0' ||
+		mlx->parse.map[i + 1][j] == '0'))
+		{
+			ft_free_map(mlx);
+			ft_return("Map not closed", mlx);
+		}
+		j--;
+	}
 }
 
 void	ft_check_mapfour(t_deflibx *mlx)
@@ -35,7 +46,7 @@ void	ft_check_mapfour(t_deflibx *mlx)
 			}
 			j--;
 		}
-		ft_check_mapfournext(mlx, i, j)
+		ft_check_mapfournext(mlx, i, j);
 		i++;
 	}
 }
@@ -52,7 +63,8 @@ void	ft_check_mapthree(t_deflibx *mlx)
 		while (mlx->parse.map[i][j] != '1' && mlx->parse.map[i][j] != '\0')
 		{
 			if ((mlx->parse.map[i][j] != ' ' &&
-			mlx->parse.map[i][j] != '\0') || (mlx->parse.map[i - 1][j] == '0' ||
+			mlx->parse.map[i][j] != '\0') ||
+			(mlx->parse.map[i - 1][j] == '0' ||
 			mlx->parse.map[i + 1][j] == '0'))
 			{
 				ft_free_map(mlx);

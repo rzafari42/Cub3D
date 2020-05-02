4c3ef4b12a0d158302240ba329246a03e83f4674
rzafari42 <redwane.zafari@hotmail.com>
2020-05-02 04:08:18 +0200

    test

--- cub3d_sprites.c
+++ cub3d_sprites.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 15:06:43 by rzafari           #+#    #+#             */
-/*   Updated: 2020/05/02 04:07:12 by marvin           ###   ########.fr       */
+/*   Updated: 2020/05/02 04:08:12 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char **argv)
 	ft_parse_arguments(&mlx, argc, argv);
 	ft_parsing(&mlx);
 	initialization(&mlx);
-	if (!(mlx->sprites.zbuffer = malloc(sizeof(double) * mlx->parse.width)))
+	if (!(mlx.sprites.zbuffer = malloc(sizeof(double) * mlx->parse.width)))
 		return (0);
 	/*if (mlx.parse.save == 1)
 	{
